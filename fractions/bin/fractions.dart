a0608d770744f3b35a800cd49fd61f7c95e4c535
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-09-13 23:06:10 -0600

    Update files

--- fractions/bin/fractions.dart
+++ fractions/bin/fractions.dart
@@ -1,5 +1,1 @@
-import 'package:fractions/fractions.dart' as fractions;
-
-void main(List<String> arguments) {
-  print('Hello world: ${fractions.calculate()}!');
-}
+void main(List<String> arguments) {}
