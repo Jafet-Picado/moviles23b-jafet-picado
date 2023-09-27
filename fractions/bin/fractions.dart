b9b949b12b81b3790d288c8da76d47a7badcd9bc
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-09-27 12:54:08 -0600

    Calculator app base project

--- fractions/bin/fractions.dart
+++ fractions/bin/fractions.dart
@@ -1,1 +1,7 @@
-void main(List<String> arguments) {}
+import 'package:fractions/arithmetic_expressions/src/arithmetic_expressions_library.dart';
+
+void main(List<String> arguments) {
+  final exp = ExpressionCalculator();
+  exp.createTree('([6/3] + 5) - -3');
+  print(exp.calculate(2));
+}
