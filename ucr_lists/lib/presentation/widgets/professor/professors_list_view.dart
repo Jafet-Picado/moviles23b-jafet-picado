beec8e7dacde6699c5b22337a3e2281092d96d14
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-10-18 15:16:37 -0600

    Create professors_list_view.dart

---
+++ ucr_lists/lib/presentation/widgets/professor/professors_list_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ProfessorsListView extends StatelessWidget {
+  const ProfessorsListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Column(children: [
+      const SizedBox(
+        height: 15,
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Row(
+          children: [
+            Text(
+              'Lista de profesores',
+              style: TextStyle(fontSize: 22, color: colors.primary),
+            ),
+          ],
+        ),
+      ),
+      const SizedBox(
+        height: 15,
+      ),
+      const Expanded(
+        child: SizedBox(),
+      ),
+      FilledButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.add),
+          label: const Text('Agregar profesor')),
+      const SizedBox(
+        height: 15,
+      ),
+    ]);
+  }
+}
