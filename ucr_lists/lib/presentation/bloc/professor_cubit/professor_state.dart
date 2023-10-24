204c4a908a5a0813a2dcc5eac56f1e726e64d23f
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-10-23 19:51:45 -0600

    Added courses list to professor state

--- ucr_lists/lib/presentation/bloc/professor_cubit/professor_state.dart
+++ ucr_lists/lib/presentation/bloc/professor_cubit/professor_state.dart
@@ -2,27 +2,31 @@ part of 'professor_cubit.dart';
 
 class ProfessorState extends Equatable {
   final List<Professor> professors;
+  final List<Course> courses;
   final int id;
   final String firstName;
   final String lastName;
 
   const ProfessorState(
       {this.professors = const [],
+      this.courses = const [],
       this.id = -1,
       this.firstName = '',
       this.lastName = ''});
 
   ProfessorState copyWith(
           {List<Professor>? professors,
+          List<Course>? courses,
           int? id,
           String? firstName,
           String? lastName}) =>
       ProfessorState(
           professors: professors ?? this.professors,
+          courses: courses ?? this.courses,
           id: id ?? this.id,
           firstName: firstName ?? this.firstName,
           lastName: lastName ?? this.lastName);
 
   @override
-  List<Object> get props => [professors, id, firstName, lastName];
+  List<Object> get props => [professors, courses, id, firstName, lastName];
 }
