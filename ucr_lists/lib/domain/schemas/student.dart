42d80c08e75b52985b560e59b7f47bfae4f6a136
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-10-23 19:52:35 -0600

    Added student schema

---
+++ ucr_lists/lib/domain/schemas/student.dart
@@ -0,0 +1,20 @@
+import 'package:isar/isar.dart';
+import 'package:ucr_lists/domain/schemas.dart';
+
+part 'student.g.dart';
+
+@collection
+class Student {
+  Id? id = Isar.autoIncrement;
+
+  @Index(unique: true)
+  String? carnet;
+
+  @Index(unique: true)
+  String? email;
+
+  String? firstName;
+  String? lastName;
+
+  final courses = IsarLinks<Course>();
+}
