8c3e3bf831e4ca2724f201f00f18b01277074b89
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-10-27 14:55:40 -0600

    Update screens.dart

--- ucr_lists/lib/presentation/screens.dart
+++ ucr_lists/lib/presentation/screens.dart
@@ -3,3 +3,5 @@ export './screens/professor/professor_add_modify_screen.dart';
 export './screens/professor/professor_details.dart';
 export './screens/course/course_add_modify_screen.dart';
 export './screens/course/course_details.dart';
+export './screens/student/student_add_modify_screen.dart';
+export './screens/student/student_details.dart';
