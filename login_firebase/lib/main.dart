41c7011658527a6ac42c7b5f1531d090137bf2ed
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-12 16:06:43 -0600

    Update main.dart

--- login_firebase/lib/main.dart
+++ login_firebase/lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:login_firebase/config/router/app_router.dart';
 import 'package:login_firebase/config/theme/theme.dart';
+import 'package:login_firebase/firebase_options.dart';
 import 'package:login_firebase/presentation/blocs.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MultiBlocProvider(
       providers: [
