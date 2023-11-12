eb624cc2f6cf557e8f9d00d4941f030eb76a6d15
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-12 16:06:49 -0600

    Update auth_state.dart

--- login_firebase/lib/presentation/blocs/auth_cubit/auth_state.dart
+++ login_firebase/lib/presentation/blocs/auth_cubit/auth_state.dart
@@ -2,18 +2,40 @@ part of 'auth_cubit.dart';
 
 class AuthState extends Equatable {
   final bool isAuth;
+  final bool isCreatingAccount;
+  final bool isLoading;
+  final bool error;
+  final String errorMessage;
+
+  final String email;
 
   const AuthState({
     this.isAuth = false,
+    this.isCreatingAccount = false,
+    this.isLoading = false,
+    this.error = false,
+    this.errorMessage = '',
+    this.email = '',
   });
 
   AuthState copyWith({
     bool? isAuth,
+    bool? isCreatingAccount,
+    bool? isLoading,
+    bool? error,
+    String? errorMessage,
+    String? email,
   }) =>
       AuthState(
         isAuth: isAuth ?? this.isAuth,
+        isCreatingAccount: isCreatingAccount ?? this.isCreatingAccount,
+        isLoading: isLoading ?? this.isLoading,
+        error: error ?? this.error,
+        errorMessage: errorMessage ?? this.errorMessage,
+        email: email ?? this.email,
       );
 
   @override
-  List<Object> get props => [isAuth];
+  List<Object> get props =>
+      [isAuth, isCreatingAccount, isLoading, error, errorMessage, email];
 }
