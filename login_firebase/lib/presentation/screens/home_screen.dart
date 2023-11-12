4d9c109d9ec088fcb1d37bbe2f12a882bc77f0b2
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-12 16:06:58 -0600

    Update home_screen.dart

--- login_firebase/lib/presentation/screens/home_screen.dart
+++ login_firebase/lib/presentation/screens/home_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:login_firebase/presentation/blocs.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final authCubit = context.watch<AuthCubit>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Página de inicio'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              authCubit.signOutUser().then((value) => context.go('/'));
+            },
+            icon: const Icon(Icons.logout_rounded),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(authCubit.state.email),
       ),
     );
   }
