ec016c5ae951760cfcb60c4f4f87f9b873750e9b
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-12 19:21:23 -0600

    Create custom_text_box.dart

---
+++ login_firebase/lib/presentation/widgets/custom_text_box.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CustomTextBox extends StatelessWidget {
+  final String fieldTitle;
+  final String fieldData;
+  final void Function()? onEditPressed;
+
+  const CustomTextBox({
+    super.key,
+    required this.fieldTitle,
+    required this.fieldData,
+    required this.onEditPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Container(
+      padding: const EdgeInsets.only(left: 15, top: 2, right: 2, bottom: 15),
+      decoration: BoxDecoration(
+        color: colors.secondaryContainer.withAlpha(50),
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: colors.secondaryContainer),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                fieldTitle,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: colors.secondary,
+                ),
+              ),
+              const Expanded(child: SizedBox()),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.edit_rounded),
+              ),
+            ],
+          ),
+          Text(fieldData),
+        ],
+      ),
+    );
+  }
+}
