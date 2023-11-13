abdef38a33dac4cbb6f10ad840df6703244dbf43
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-12 19:50:58 -0600

    Update firestore_service.dart

--- login_firebase/lib/infrastructure/services/firestore_service.dart
+++ login_firebase/lib/infrastructure/services/firestore_service.dart
@@ -33,6 +33,30 @@ class FirestoreService {
     }
   }
 
+  Future<void> createUserData(
+      String collectionPath, String email, String username, String bio) async {
+    await FirebaseFirestore.instance.collection(collectionPath).doc(email).set({
+      'username': username,
+      'bio': bio,
+    });
+  }
+
+  Future<void> updateUserData(
+      String collectionPath, String email, String field, String data) async {
+    await FirebaseFirestore.instance
+        .collection(collectionPath)
+        .doc(email)
+        .update({field: data});
+  }
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserData(
+      String collectionPath, String email) async {
+    return FirebaseFirestore.instance
+        .collection(collectionPath)
+        .doc(email)
+        .get();
+  }
+
   void clear() async {
     await FirebaseFirestore.instance.terminate();
     await FirebaseFirestore.instance.clearPersistence();
