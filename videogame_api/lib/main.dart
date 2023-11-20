29886b0232ee8530021a629b68974df35855080f
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-20 15:21:54 -0600

    Update main.dart

--- videogame_api/lib/main.dart
+++ videogame_api/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:videogame_api/config/router/app_router.dart';
 import 'package:videogame_api/config/theme/app_theme.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(child: MainApp()),
+  );
 }
 
 class MainApp extends StatelessWidget {
