071ac542e60c1db38b3bc5b4bb2c7efc9849cc5b
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-20 11:53:29 -0600

    Update profile_screen.dart

--- login_firebase/lib/presentation/screens/profile_screen.dart
+++ login_firebase/lib/presentation/screens/profile_screen.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:login_firebase/presentation/blocs.dart';
 import 'package:login_firebase/presentation/widgets.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  void editFieldDialog(
+      String text, AuthCubit authCubit, String field, String email) {
+    String newValue = '';
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(
+            'Editar $text',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+          ),
+          content: TextField(
+            autofocus: true,
+            decoration: InputDecoration(
+              hintText: 'Modifique el $text',
+            ),
+            onChanged: (value) {
+              newValue = value;
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                context.pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            FilledButton(
+              onPressed: () {
+                authCubit.updateUserData(email, field, newValue);
+                context.pop();
+              },
+              child: const Text('Guardar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +61,8 @@ class ProfileScreen extends StatelessWidget {
 
     final authCubit = context.watch<AuthCubit>();
     final email = context.watch<AuthCubit>().state.email;
+    final username = context.watch<AuthCubit>().state.username;
+    final bio = context.watch<AuthCubit>().state.bio;
 
     return Scaffold(
       appBar: AppBar(
@@ -46,14 +97,28 @@ class ProfileScreen extends StatelessWidget {
             const SizedBox(height: 15),
             CustomTextBox(
               fieldTitle: 'Nombre de usuario',
-              fieldData: 'Jafet',
-              onEditPressed: () {},
+              fieldData: username,
+              onEditPressed: () {
+                editFieldDialog(
+                  'nombre de usuario',
+                  authCubit,
+                  'username',
+                  email,
+                );
+              },
             ),
             const SizedBox(height: 15),
             CustomTextBox(
               fieldTitle: 'Biografía',
-              fieldData: 'Soy un estudiante y me gusta Flutter!',
-              onEditPressed: () {},
+              fieldData: bio,
+              onEditPressed: () {
+                editFieldDialog(
+                  'biografía',
+                  authCubit,
+                  'bio',
+                  email,
+                );
+              },
             ),
           ],
         ),
