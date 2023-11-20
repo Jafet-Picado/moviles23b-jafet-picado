a5a61ecbd4c96c0ddfbbfd49cee9aee9e06b61ee
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-20 15:32:18 -0600

    Update home_screen.dart

--- videogame_api/lib/presentation/screens/home_screen.dart
+++ videogame_api/lib/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:videogame_api/presentation/providers/video_games_provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,6 +13,53 @@ class HomeScreen extends StatelessWidget {
           'My videogames',
         ),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(videoGamesProvider.notifier).loadVideoGamesInfo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final videoGamesInfo = ref.watch(videoGamesProvider);
+    return ListView.builder(
+      itemCount: videoGamesInfo.length,
+      itemBuilder: (context, index) {
+        final videoGame = videoGamesInfo[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: ListBody(
+            children: [
+              Row(
+                children: [
+                  Image.network(
+                    videoGame.sampleCover.thumbnailImage,
+                    width: 100,
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Text(videoGame.title),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        );
+      },
     );
   }
 }
