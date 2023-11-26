4f28a9d59a591556b5e4eb4ad20be22c5e177773
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-25 18:57:55 -0600

    Update screens.dart

--- hero_cards/lib/presentation/screens.dart
+++ hero_cards/lib/presentation/screens.dart
@@ -1,3 +1,5 @@
 export './screens/home_screen.dart';
 export './screens/login_screen.dart';
 export './screens/register_screen.dart';
+export './screens/big_hero_card_screen.dart';
+export './screens/hero_info_screen.dart';
