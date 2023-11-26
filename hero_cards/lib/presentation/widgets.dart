80b5b90a9e336bb7f533fc965cdb32f6a46f15da
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-25 18:57:40 -0600

    Update widgets.dart

--- hero_cards/lib/presentation/widgets.dart
+++ hero_cards/lib/presentation/widgets.dart
@@ -1,2 +1,5 @@
 export './widgets/custom_text_field.dart';
 export './widgets/custom_button.dart';
+export './widgets/big_hero_card.dart';
+export './widgets/inventory_list_view.dart';
+export './widgets/custom_list_card.dart';
