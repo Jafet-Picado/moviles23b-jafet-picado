e5161ee1bf27f75f24580fe99a43397b990b8a46
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-26 23:02:54 -0600

    Update superhero_datasource.dart

--- hero_cards/lib/infrastructure/datasources/superhero_datasource.dart
+++ hero_cards/lib/infrastructure/datasources/superhero_datasource.dart
@@ -1,8 +1,14 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hero_cards/domain/datasources/hero_datasource.dart';
 import 'package:hero_cards/domain/entities/hero_info.dart';
+import 'package:hero_cards/domain/entities/hero_info_detailed.dart';
+import 'package:hero_cards/domain/entities/hero_info_minimal.dart';
+import 'package:hero_cards/infrastructure/mappers/hero_info_detailed_mapper.dart';
 import 'package:hero_cards/infrastructure/mappers/hero_info_mapper.dart';
+import 'package:hero_cards/infrastructure/mappers/hero_info_minimal_mapper.dart';
 import 'package:hero_cards/infrastructure/models/superhero_response.dart';
 
 class SuperheroDatasource extends HeroDatasource {
@@ -19,6 +25,60 @@ class SuperheroDatasource extends HeroDatasource {
     final heroResponse = SuperheroResponse.fromJson(response.data);
     final HeroInfo hero = HeroInfoMapper().superheroToEntity(heroResponse);
 
+    return hero;
+  }
+
+  @override
+  Future<HeroInfoDetailed> getHeroDetailed({required int id}) async {
+    final dio = Dio(BaseOptions(
+        baseUrl:
+            'https://comicvine.gamespot.com/api/character/4005-$id/?api_key='));
+
+    final apiKey = dotenv.env['API_KEY'];
+    final response = await dio.get(
+      '$apiKey&format=json',
+    );
+
+    final HeroInfoDetailed hero = HeroInfoDetailedMapper()
+        .superheroToEntityJSON(response.data['results']);
+    return hero;
+  }
+
+  @override
+  Future<HeroInfoMinimal> getRandomHero() async {
+    int offset = Random().nextInt(158505);
+
+    final apiKey = dotenv.env['API_KEY'];
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://comicvine.gamespot.com/api/characters',
+      ),
+    );
+
+    final response = await dio.get(
+      '/?api_key=$apiKey&format=json&limit=1&offset=$offset',
+    );
+
+    final HeroInfoMinimal hero = HeroInfoMinimalMapper()
+        .superheroToEntityJSON(response.data['results'][0]);
+    return hero;
+  }
+
+  @override
+  Future<HeroInfoMinimal> getHeroMinimal({required int id}) async {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://comicvine.gamespot.com/api/characters',
+      ),
+    );
+
+    final apiKey = dotenv.env['API_KEY'];
+    final response = await dio.get(
+      '/?api_key=$apiKey&format=json&limit=1&filter=id:$id',
+    );
+
+    final HeroInfoMinimal hero = HeroInfoMinimalMapper()
+        .superheroToEntityJSON(response.data['results'][0]);
     return hero;
   }
 }
