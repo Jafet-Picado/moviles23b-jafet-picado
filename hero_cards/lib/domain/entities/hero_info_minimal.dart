71ac120477a48c13fdf86b2c513b07428ec4333a
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-26 23:02:49 -0600

    Create hero_info_minimal.dart

---
+++ hero_cards/lib/domain/entities/hero_info_minimal.dart
@@ -0,0 +1,39 @@
+class HeroInfoMinimal {
+  int id;
+  EntityImage image;
+  String name;
+  String publisher;
+
+  HeroInfoMinimal({
+    required this.id,
+    required this.image,
+    required this.name,
+    required this.publisher,
+  });
+}
+
+class EntityImage {
+  String iconUrl;
+  String mediumUrl;
+  String screenUrl;
+  String screenLargeUrl;
+  String smallUrl;
+  String superUrl;
+  String thumbUrl;
+  String tinyUrl;
+  String originalUrl;
+  String imageTags;
+
+  EntityImage({
+    required this.iconUrl,
+    required this.mediumUrl,
+    required this.screenUrl,
+    required this.screenLargeUrl,
+    required this.smallUrl,
+    required this.superUrl,
+    required this.thumbUrl,
+    required this.tinyUrl,
+    required this.originalUrl,
+    required this.imageTags,
+  });
+}
