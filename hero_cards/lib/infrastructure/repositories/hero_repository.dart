a5aea4db2ba525a00bf1f872c684b33f2d398bcd
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-26 23:03:05 -0600

    Update hero_repository.dart

--- hero_cards/lib/infrastructure/repositories/hero_repository.dart
+++ hero_cards/lib/infrastructure/repositories/hero_repository.dart
@@ -1,5 +1,7 @@
 import 'package:hero_cards/domain/datasources/hero_datasource.dart';
 import 'package:hero_cards/domain/entities/hero_info.dart';
+import 'package:hero_cards/domain/entities/hero_info_detailed.dart';
+import 'package:hero_cards/domain/entities/hero_info_minimal.dart';
 import 'package:hero_cards/domain/repositories/hero_repository.dart';
 
 class HeroRepositoryImpl extends HeroRepository {
@@ -10,5 +12,20 @@ class HeroRepositoryImpl extends HeroRepository {
   @override
   Future<HeroInfo> getHero({required int id}) async {
     return datasource.getHero(id: id);
+  }
+
+  @override
+  Future<HeroInfoDetailed> getHeroDetailed({required int id}) {
+    return datasource.getHeroDetailed(id: id);
+  }
+
+  @override
+  Future<HeroInfoMinimal> getRandomHero() {
+    return datasource.getRandomHero();
+  }
+
+  @override
+  Future<HeroInfoMinimal> getHeroMinimal({required int id}) {
+    return datasource.getHeroMinimal(id: id);
   }
 }
