3cf18c2b69a15ba56a78f4c26ff7c5bc4059485a
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-26 23:02:43 -0600

    Update hero_datasource.dart

--- hero_cards/lib/domain/datasources/hero_datasource.dart
+++ hero_cards/lib/domain/datasources/hero_datasource.dart
@@ -1,5 +1,10 @@
 import 'package:hero_cards/domain/entities/hero_info.dart';
+import 'package:hero_cards/domain/entities/hero_info_detailed.dart';
+import 'package:hero_cards/domain/entities/hero_info_minimal.dart';
 
 abstract class HeroDatasource {
   Future<HeroInfo> getHero({required int id});
+  Future<HeroInfoDetailed> getHeroDetailed({required int id});
+  Future<HeroInfoMinimal> getRandomHero();
+  Future<HeroInfoMinimal> getHeroMinimal({required int id});
 }
