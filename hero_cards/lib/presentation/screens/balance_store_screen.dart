7182ce67d3c38c655e2f26cc3ec3ff71d1529579
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-27 17:24:39 -0600

    Update balance_store_screen.dart

--- hero_cards/lib/presentation/screens/balance_store_screen.dart
+++ hero_cards/lib/presentation/screens/balance_store_screen.dart
@@ -6,20 +6,21 @@ import 'package:hero_cards/presentation/widgets.dart';
 class BalanceStoreScreen extends StatelessWidget {
   const BalanceStoreScreen({super.key});
 
-  void _showWarning(ColorScheme colors, BuildContext context) {
+  void _showDialog(
+      ColorScheme colors, BuildContext context, String message, IconData icon) {
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           icon: Icon(
-            Icons.warning_rounded,
+            icon,
             color: colors.onBackground,
             size: 50,
           ),
-          content: const Text(
-            'Ya se encuentra en la tienda',
+          content: Text(
+            message,
             textAlign: TextAlign.center,
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
             ),
@@ -29,9 +30,30 @@ class BalanceStoreScreen extends StatelessWidget {
     );
   }
 
+  void _buyCoins(BuildContext context, AuthCubit authCubit, ColorScheme colors,
+      int amount) {
+    if ((authCubit.state.balance + amount) > 100000) {
+      _showDialog(
+        colors,
+        context,
+        'La cantidad máxima de HeroCoins es de 100.000',
+        Icons.warning_rounded,
+      );
+    } else {
+      authCubit.updateBalance(amount: amount);
+      _showDialog(
+        colors,
+        context,
+        'Compra exitosa',
+        Icons.check_circle_rounded,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
+    final authCubit = context.read<AuthCubit>();
 
     return Scaffold(
       appBar: AppBar(
@@ -45,30 +67,81 @@ class BalanceStoreScreen extends StatelessWidget {
           CustomPillButton(
             balance: context.watch<AuthCubit>().state.balance.toString(),
             onTap: () {
-              _showWarning(colors, context);
+              _showDialog(
+                colors,
+                context,
+                'Ya se encuentra en la tienda',
+                Icons.warning_rounded,
+              );
             },
           ),
           const SizedBox(width: 20),
         ],
       ),
-      body: const Column(
-        children: [
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Tienda de HeroCoins',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Horizon',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Tienda de HeroCoins',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Horizon',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+            const SizedBox(height: 20),
+            BalanceStoreCard(
+              image: 'https://img.icons8.com/color/96/batman-old.png',
+              title: 'nocturnal',
+              amount: '1000',
+              price: '2500',
+              titleColor: Colors.blueGrey,
+              onTap: () {
+                _buyCoins(context, authCubit, colors, 1000);
+              },
+            ),
+            BalanceStoreCard(
+              image: 'https://img.icons8.com/color/96/superman.png',
+              title: 'super',
+              amount: '2500',
+              price: '6000',
+              titleColor: Colors.lightBlue,
+              onTap: () {
+                _buyCoins(context, authCubit, colors, 2500);
+              },
+            ),
+            BalanceStoreCard(
+              image: 'https://img.icons8.com/ios-filled/100/x-men.png',
+              title: 'mutant',
+              amount: '5000',
+              price: '10000',
+              iconColor: Colors.yellow,
+              titleColor: Colors.yellow,
+              onTap: () {
+                _buyCoins(context, authCubit, colors, 5000);
+              },
+            ),
+            BalanceStoreCard(
+              image: 'https://img.icons8.com/ios/100/000000/spiderman-new.png',
+              title: 'spider',
+              amount: '10000',
+              price: '20000',
+              iconColor: Colors.red,
+              titleColor: Colors.red,
+              onTap: () {
+                _buyCoins(context, authCubit, colors, 10000);
+              },
+            ),
+            const SizedBox(height: 50),
+          ],
+        ),
       ),
     );
   }
