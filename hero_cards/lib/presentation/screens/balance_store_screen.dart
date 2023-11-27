57b80a145816d3b9aab5030bb00bf625c06309aa
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-27 15:05:53 -0600

    Update balance_store_screen.dart

--- hero_cards/lib/presentation/screens/balance_store_screen.dart
+++ hero_cards/lib/presentation/screens/balance_store_screen.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hero_cards/presentation/blocs/auth_cubit/auth_cubit.dart';
 import 'package:hero_cards/presentation/widgets.dart';
 
 class BalanceStoreScreen extends StatelessWidget {
   const BalanceStoreScreen({super.key});
+
+  void _showWarning(ColorScheme colors, BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          icon: Icon(
+            Icons.warning_rounded,
+            color: colors.onBackground,
+            size: 50,
+          ),
+          content: const Text(
+            'Ya se encuentra en la tienda',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,32 +43,31 @@ class BalanceStoreScreen extends StatelessWidget {
         ),
         actions: [
           CustomPillButton(
-            balance:
-                '1000', //context.watch<AuthCubit>().state.balance.toString(),
+            balance: context.watch<AuthCubit>().state.balance.toString(),
             onTap: () {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    icon: Icon(
-                      Icons.warning_rounded,
-                      color: colors.onBackground,
-                      size: 50,
-                    ),
-                    content: const Text(
-                      'Ya se encuentra en la tienda',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  );
-                },
-              );
+              _showWarning(colors, context);
             },
           ),
           const SizedBox(width: 20),
+        ],
+      ),
+      body: const Column(
+        children: [
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Tienda de HeroCoins',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Horizon',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
