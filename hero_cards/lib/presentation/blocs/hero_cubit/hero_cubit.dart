47b7d764236f66eae3e01402a0ab8e564acff6a9
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-11-26 23:03:13 -0600

    Update hero_cubit.dart

--- hero_cards/lib/presentation/blocs/hero_cubit/hero_cubit.dart
+++ hero_cards/lib/presentation/blocs/hero_cubit/hero_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:hero_cards/domain/entities/hero_info.dart';
+import 'package:hero_cards/domain/entities/hero_info_detailed.dart';
+import 'package:hero_cards/domain/entities/hero_info_minimal.dart';
 import 'package:hero_cards/infrastructure/datasources/superhero_datasource.dart';
 import 'package:hero_cards/infrastructure/repositories/hero_repository.dart';
 
@@ -29,6 +31,43 @@ class HeroCubit extends Cubit<HeroState> {
     }
   }
 
+  Future<void> getHeroMinimalRandom() async {
+    try {
+      emit(state.copyWith(isLoading: true));
+      HeroRepositoryImpl repo =
+          HeroRepositoryImpl(datasource: SuperheroDatasource());
+      HeroInfoMinimal hero = await repo.getRandomHero();
+
+      emit(state.copyWith(
+        isLoading: false,
+        heroMinimal: hero,
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+        error: true,
+        errorMessage: e.toString(),
+      ));
+    }
+  }
+
+  Future<void> getHeroDetailedById({required int id}) async {
+    try {
+      emit(state.copyWith(isLoading: true));
+      HeroRepositoryImpl repo =
+          HeroRepositoryImpl(datasource: SuperheroDatasource());
+      HeroInfoDetailed hero = await repo.getHeroDetailed(id: id);
+      emit(state.copyWith(
+        isLoading: false,
+        heroDetailed: hero,
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+        error: true,
+        errorMessage: e.toString(),
+      ));
+    }
+  }
+
   Future<void> getHeroesByList({required List<int> heroes}) async {
     try {
       emit(state.copyWith(isLoading: true));
@@ -42,6 +81,54 @@ class HeroCubit extends Cubit<HeroState> {
       emit(state.copyWith(
         isLoading: false,
         heroes: heroesList,
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+        error: true,
+        errorMessage: e.toString(),
+      ));
+    }
+  }
+
+  Future<void> getHeroesDetailedByList({required List<int> heroes}) async {
+    try {
+      emit(state.copyWith(isLoading: true));
+      HeroRepositoryImpl repo =
+          HeroRepositoryImpl(datasource: SuperheroDatasource());
+      List<HeroInfoDetailed> heroesList = [];
+      for (int id in heroes) {
+        await repo
+            .getHeroDetailed(id: id)
+            .then((value) => heroesList.add(value));
+      }
+
+      emit(state.copyWith(
+        isLoading: false,
+        heroesDetailed: heroesList,
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+        error: true,
+        errorMessage: e.toString(),
+      ));
+    }
+  }
+
+  Future<void> getHeroesMinimalByList({required List<int> heroes}) async {
+    try {
+      emit(state.copyWith(isLoading: true));
+      HeroRepositoryImpl repo =
+          HeroRepositoryImpl(datasource: SuperheroDatasource());
+      List<HeroInfoMinimal> heroesList = [];
+      for (int id in heroes) {
+        await repo
+            .getHeroMinimal(id: id)
+            .then((value) => heroesList.add(value));
+      }
+
+      emit(state.copyWith(
+        isLoading: false,
+        heroesMinimal: heroesList,
       ));
     } catch (e) {
       emit(state.copyWith(
