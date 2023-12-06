a12d99a4ac5e74748ef19535b8ff63032bc61ded
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-12-06 16:48:52 -0600

    Update hero_info_screen.dart

--- hero_cards/lib/presentation/screens/hero_screens/hero_info_screen.dart
+++ hero_cards/lib/presentation/screens/hero_screens/hero_info_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hero_cards/domain/entities/hero_info_detailed.dart';
 import 'package:hero_cards/presentation/blocs/hero_cubit/hero_cubit.dart';
 import 'package:hero_cards/presentation/widgets.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:photo_view/photo_view.dart';
 
 class HeroInfoScreen extends StatelessWidget {
@@ -34,6 +34,23 @@ class _HeroInfoScreen extends StatefulWidget {
 }
 
 class _HeroInfoScreenState extends State<_HeroInfoScreen> {
+  List<Widget> _buildColumn(List<EntityFirstAppearedInIssue> list) {
+    return list.map((friend) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            friend.name!,
+            style: const TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      );
+    }).toList();
+  }
+
   @override
   void initState() {
     super.initState();
@@ -43,7 +60,7 @@ class _HeroInfoScreenState extends State<_HeroInfoScreen> {
   @override
   Widget build(BuildContext context) {
     final hero = context.read<HeroCubit>().state.heroDetailed;
-    final colors = Theme.of(context).colorScheme;
+
     return (!context.watch<HeroCubit>().state.isLoading)
         ? Scaffold(
             appBar: AppBar(
@@ -65,51 +82,187 @@ class _HeroInfoScreenState extends State<_HeroInfoScreen> {
                 icon: Icons.arrow_back_rounded,
               ),
             ),
-            body: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 10),
-                    GestureDetector(
-                      onTap: () {
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return GestureDetector(
-                                onTap: () {
-                                  Navigator.of(context, rootNavigator: true)
-                                      .pop(this);
-                                },
-                                child: PhotoView(
-                                  imageProvider: Image.network(
-                                    hero.image.mediumUrl,
-                                  ).image,
-                                ),
-                              );
-                            });
-                      },
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.network(
-                          hero.image.mediumUrl,
-                          fit: BoxFit.cover,
-                        ),
+            body: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.of(context, rootNavigator: true)
+                                    .pop(this);
+                              },
+                              child: PhotoView(
+                                imageProvider: Image.network(
+                                  hero.image.mediumUrl,
+                                ).image,
+                              ),
+                            );
+                          });
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.network(
+                        hero.image.mediumUrl,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Image.network(
+                            hero.image.smallUrl,
+                            fit: BoxFit.cover,
+                            errorBuilder: (context, error, stackTrace) {
+                              return Image.asset('assets/images/noimagen.jpg');
+                            },
+                          );
+                        },
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    (hero.deck == '') ? '' : '"${hero.deck}"',
+                    style: const TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Nombre real: ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Horizon',
+                        ),
+                      ),
+                      Text(
+                        (hero.realName == "") ? 'Desconocido' : hero.realName,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Raza: ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Horizon',
+                        ),
+                      ),
+                      Text(
+                        (hero.origin.name == "")
+                            ? 'Desconocido'
+                            : hero.origin.name!,
+                        style: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  const Text(
+                    'Aliados: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Horizon',
+                    ),
+                  ),
+                  (hero.characterFriends.isEmpty)
+                      ? const Text(
+                          'Desconocido',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      : Column(
+                          children: [
+                            ..._buildColumn(hero.characterFriends),
+                          ],
+                        ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Enemigos: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Horizon',
+                    ),
+                  ),
+                  (hero.characterFriends.isEmpty)
+                      ? const Text(
+                          'Desconocido',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      : Column(
+                          children: [
+                            ..._buildColumn(hero.characterEnemies),
+                          ],
+                        ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Poderes: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Horizon',
+                    ),
+                  ),
+                  (hero.powers.isEmpty)
+                      ? const Text(
+                          'Desconocido',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      : Column(
+                          children: [
+                            ..._buildColumn(hero.powers),
+                          ],
+                        ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Publisher: ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Horizon',
+                        ),
+                      ),
+                      Text(
+                        (hero.publisher.name == "")
+                            ? 'Desconocido'
+                            : hero.publisher.name!,
+                        style: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                ],
               ),
             ),
           )
-        : Scaffold(
-            body: Center(
-              child: LoadingAnimationWidget.fourRotatingDots(
-                color: colors.primary,
-                size: 100,
-              ),
-            ),
-          );
+        : const CustomLoadingWidget();
   }
 }
