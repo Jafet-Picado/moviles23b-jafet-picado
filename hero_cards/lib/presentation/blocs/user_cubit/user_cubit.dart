922467182b542c431820fdab985e73a2df4223a4
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-12-06 14:06:18 -0600

    Update user_cubit.dart

--- hero_cards/lib/presentation/blocs/user_cubit/user_cubit.dart
+++ hero_cards/lib/presentation/blocs/user_cubit/user_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:hero_cards/infrastructure/services/firestore_service.dart';
@@ -9,7 +10,23 @@ class UserCubit extends Cubit<UserState> {
 
   Future<void> getUsers() async {
     emit(state.copyWith(isLoading: true));
-    final querySnapshot = await FirestoreService().getUsers('users');
-    emit(state.copyWith(isLoading: false, users: querySnapshot.docs));
+    final querySnapshot = await FirestoreService().getUsers();
+    List<Map<String, dynamic>> users =
+        querySnapshot.docs.map((QueryDocumentSnapshot documentSnapshot) {
+      String username = documentSnapshot['username'];
+      String bio = documentSnapshot['bio'];
+      int balance = documentSnapshot['balance'];
+      bool isOnline = documentSnapshot['isOnline'];
+      List<dynamic> cards = documentSnapshot['cards'];
+      final map = <String, dynamic>{
+        'username': username,
+        'bio': bio,
+        'balance': balance,
+        'isOnline': isOnline,
+        'cards': cards,
+      };
+      return map;
+    }).toList();
+    emit(state.copyWith(isLoading: false, users: users));
   }
 }
