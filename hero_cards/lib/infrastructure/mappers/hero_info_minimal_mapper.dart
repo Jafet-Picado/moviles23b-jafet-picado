78b7be0e2146bd46e8c744b2bea100c1bfb28e99
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-12-06 20:35:10 -0600

    Update hero_info_minimal_mapper.dart

--- hero_cards/lib/infrastructure/mappers/hero_info_minimal_mapper.dart
+++ hero_cards/lib/infrastructure/mappers/hero_info_minimal_mapper.dart
@@ -1,6 +1,10 @@
 import 'package:hero_cards/domain/entities/hero_info_minimal.dart';
 
+//Clase encargada de mapear la respuesta del API (JSON) a la clase HeroInfoDetailed
 class HeroInfoMinimalMapper {
+  //Recibe un JSON con la información mínima de un héroe y convierte esa
+  //información en una instancia de la clase HeroInfoMinimal al iterar por cada
+  //campo y asignando los valores correspondientes.
   HeroInfoMinimal superheroToEntityJSON(Map<String, dynamic> json) =>
       HeroInfoMinimal(
         id: json['id'] ?? '',
