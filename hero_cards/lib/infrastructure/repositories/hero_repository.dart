196d368d79f053898d941bc8a4cf384f346d2aa0
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-12-06 20:35:18 -0600

    Update hero_repository.dart

--- hero_cards/lib/infrastructure/repositories/hero_repository.dart
+++ hero_cards/lib/infrastructure/repositories/hero_repository.dart
@@ -1,29 +1,30 @@
 import 'package:hero_cards/domain/datasources/hero_datasource.dart';
-import 'package:hero_cards/domain/entities/hero_info.dart';
 import 'package:hero_cards/domain/entities/hero_info_detailed.dart';
 import 'package:hero_cards/domain/entities/hero_info_minimal.dart';
 import 'package:hero_cards/domain/repositories/hero_repository.dart';
 
+//Implementación de la clase abstracta del repositorio de héroes
 class HeroRepositoryImpl extends HeroRepository {
   final HeroDatasource datasource;
 
   HeroRepositoryImpl({required this.datasource});
 
-  @override
-  Future<HeroInfo> getHero({required int id}) async {
-    return datasource.getHero(id: id);
-  }
-
+  //Obtiene la información detallada de un héroe con su ID al llamar al método
+  //correspondiente en la clase de fuente de datos.
   @override
   Future<HeroInfoDetailed> getHeroDetailed({required int id}) {
     return datasource.getHeroDetailed(id: id);
   }
 
+  //Obtiene la información mínima de un héroe aleatorio al llamar al método
+  //correspondiente en la clase de fuente de datos.
   @override
   Future<HeroInfoMinimal> getRandomHero() {
     return datasource.getRandomHero();
   }
 
+  //Obtiene la información mínima de un héroe con su ID al llamar al método
+  //correspondiente en la clase de fuente de datos.
   @override
   Future<HeroInfoMinimal> getHeroMinimal({required int id}) {
     return datasource.getHeroMinimal(id: id);
