b9d8a9f2639d93ab232427b696d5d42ec0e837f5
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-12-06 20:35:42 -0600

    Update hero_cubit.dart

--- hero_cards/lib/presentation/blocs/hero_cubit/hero_cubit.dart
+++ hero_cards/lib/presentation/blocs/hero_cubit/hero_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:hero_cards/domain/entities/hero_info.dart';
 import 'package:hero_cards/domain/entities/hero_info_detailed.dart';
 import 'package:hero_cards/domain/entities/hero_info_minimal.dart';
 import 'package:hero_cards/infrastructure/datasources/superhero_datasource.dart';
@@ -11,26 +10,8 @@ part 'hero_state.dart';
 class HeroCubit extends Cubit<HeroState> {
   HeroCubit() : super(const HeroState());
 
-  Future<void> getHeroById({required int id}) async {
-    try {
-      emit(state.copyWith(isLoading: true));
-      HeroRepositoryImpl repo =
-          HeroRepositoryImpl(datasource: SuperheroDatasource());
-      HeroInfo? hero;
-      await repo.getHero(id: id).then((value) => hero = value);
-
-      emit(state.copyWith(
-        isLoading: false,
-        hero: hero,
-      ));
-    } catch (e) {
-      emit(state.copyWith(
-        error: true,
-        errorMessage: e.toString(),
-      ));
-    }
-  }
-
+  //Obtiene la información mínima de un héroe de forma aleatoria y actualiza
+  //el estado para tener la información disponible
   Future<void> getHeroMinimalRandom() async {
     try {
       emit(state.copyWith(isLoading: true));
@@ -50,6 +31,8 @@ class HeroCubit extends Cubit<HeroState> {
     }
   }
 
+  //Obtiene la información detallada de un héroe por medio de su ID y actualiza
+  //el estado para tener su información disponible
   Future<void> getHeroDetailedById({required int id}) async {
     try {
       emit(state.copyWith(isLoading: true));
@@ -68,28 +51,8 @@ class HeroCubit extends Cubit<HeroState> {
     }
   }
 
-  Future<void> getHeroesByList({required List<int> heroes}) async {
-    try {
-      emit(state.copyWith(isLoading: true));
-      HeroRepositoryImpl repo =
-          HeroRepositoryImpl(datasource: SuperheroDatasource());
-      List<HeroInfo> heroesList = [];
-      for (int id in heroes) {
-        await repo.getHero(id: id).then((value) => heroesList.add(value));
-      }
-
-      emit(state.copyWith(
-        isLoading: false,
-        heroes: heroesList,
-      ));
-    } catch (e) {
-      emit(state.copyWith(
-        error: true,
-        errorMessage: e.toString(),
-      ));
-    }
-  }
-
+  //Obtiene la información detallada de una lista de héroes y actualiza el estado
+  //para tener la información disponible
   Future<void> getHeroesDetailedByList({required List<int> heroes}) async {
     try {
       emit(state.copyWith(isLoading: true));
@@ -114,6 +77,8 @@ class HeroCubit extends Cubit<HeroState> {
     }
   }
 
+  //Obtiene la información mínima de una lista de héroes y actualiza el estado
+  //para tener la información disponible
   Future<void> getHeroesMinimalByList({required List<int> heroes}) async {
     try {
       emit(state.copyWith(isLoading: true));
@@ -138,6 +103,8 @@ class HeroCubit extends Cubit<HeroState> {
     }
   }
 
+  //Obtiene la información de una lista de héroes aleatorios y actualiza el
+  //estado para tener la información disponible
   Future<void> getRandomHeroesMinimal({int quantity = 8}) async {
     try {
       emit(state.copyWith(isLoading: true));
