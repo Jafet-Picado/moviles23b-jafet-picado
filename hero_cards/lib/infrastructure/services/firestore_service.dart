23b55193655d26db9412fe5594fcd3f742e42bfe
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-12-06 20:35:22 -0600

    Update firestore_service.dart

--- hero_cards/lib/infrastructure/services/firestore_service.dart
+++ hero_cards/lib/infrastructure/services/firestore_service.dart
@@ -1,6 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//Clase encargada de manejar los servicios de almacenamiento y autenticación
+//con Firebase
 class FirestoreService {
+  //Crea la información personal de cada usuario, esto incluye: nombre de usuario,
+  //biografía, lista de IDs de sus cartas, cantidad de monedas actuales y su estado
+  //en la aplicación (Online/Offline)
   Future<void> createUserData(
       String collectionPath, String email, String username, String bio) async {
     await FirebaseFirestore.instance.collection(collectionPath).doc(email).set({
@@ -12,6 +17,8 @@ class FirestoreService {
     });
   }
 
+  //Actualiza un campo especifico de la información de un usuario al modificar
+  //el documento donde se almacena dicha información.
   Future<void> updateUserData(
       String collectionPath, String email, String field, dynamic data) async {
     await FirebaseFirestore.instance
@@ -20,6 +27,7 @@ class FirestoreService {
         .update({field: data});
   }
 
+  //Añade el ID de una carta a la lista de cartas del usuario
   Future<void> addUserCard(String collectionPath, String email, int id) async {
     await FirebaseFirestore.instance
         .collection(collectionPath)
@@ -29,6 +37,7 @@ class FirestoreService {
     });
   }
 
+  //Retorna el documento con la información completa del usuario búscado
   Future<DocumentSnapshot<Map<String, dynamic>>> getUserData(
       String collectionPath, String email) async {
     return FirebaseFirestore.instance
@@ -37,12 +46,14 @@ class FirestoreService {
         .get();
   }
 
+  //Retorna todos los documentos de la colección de usuarios
   Future<QuerySnapshot> getUsers() async {
     final CollectionReference usersCollection =
         FirebaseFirestore.instance.collection('users');
     return await usersCollection.get();
   }
 
+  //Actualiza el monedero del usuario
   Future<void> updateUserBalance(
       String collectionPath, String email, int balance) async {
     await FirebaseFirestore.instance
@@ -53,6 +64,8 @@ class FirestoreService {
     });
   }
 
+  //Este método se encarga de terminar la instancia de Firebase utilizada y
+  //además elimina los datos persistente de manera local.
   void clear() async {
     await FirebaseFirestore.instance.terminate();
     await FirebaseFirestore.instance.clearPersistence();
