bdd01993b1f3c90d13cb9184f171212d56d4343c
Jafet Picado <82168213+Jafet-Picado@users.noreply.github.com>
2023-12-06 20:34:54 -0600

    Update hero_repository.dart

--- hero_cards/lib/domain/repositories/hero_repository.dart
+++ hero_cards/lib/domain/repositories/hero_repository.dart
@@ -1,10 +1,13 @@
-import 'package:hero_cards/domain/entities/hero_info.dart';
 import 'package:hero_cards/domain/entities/hero_info_detailed.dart';
 import 'package:hero_cards/domain/entities/hero_info_minimal.dart';
 
+//Clase abstracta de un repositorio para manejar la obtención de información
+//de los héroes.
 abstract class HeroRepository {
-  Future<HeroInfo> getHero({required int id});
+  //Obtener la información detallada de un héroe con su ID
   Future<HeroInfoDetailed> getHeroDetailed({required int id});
+  //Obtener información minima de un héroe aleatorio
   Future<HeroInfoMinimal> getRandomHero();
+  //Obtener la información mínima de un héroe con su ID
   Future<HeroInfoMinimal> getHeroMinimal({required int id});
 }
